ce5f0bb555546910ff8527734ec38ba2dc5aa208
Wilfrido rondon <wilfridorondon14@gmail.comm>
2021-05-09 21:47:56 -0400

    Commit 20210509

--- 8-funciones/main.py
+++ 8-funciones/main.py
@@ -47,3 +47,58 @@ def mostrarTuNombre(nombre, edad):
 nombre = input("Cual es tu nombre: ")
 edad = int(input("Cual es tu edad: "))
 mostrarTuNombre(nombre, edad)
+
+
+#ejmeplo tabla de multiplicar
+
+def tablaMultiplicar(numero):
+    print(f"Tabla de multiplicar del numero: {numero}")
+
+    for contador in range(1,11):
+        print(f"{numero} x {contador} = {numero  * contador}")
+
+    print("\n")
+
+
+for numero_tabla in range(1,11):
+    tablaMultiplicar(numero_tabla)
+    print("\n")
+
+
+    #ejemplo asercar de los parametros opcionales
+
+    def getEmpleados(nombre, dni = None):
+        print("EMPLEADO")
+        print(f"Nombre: {nombre}")
+
+        if dni != None:
+            print(f"DNI: {dni}") 
+
+getEmpleados("wilfrido Rondon", 222542)
+
+
+print ("\n")
+print("EJEMPLO DE FUNCION UTILIZANDO RETURN")
+
+def calculadora(numero1, numero2, basico = False):
+
+    suma = numero1 +  numero2
+    resta = numero1 - numero2
+    multi = numero1 *  numero2
+    divi = numero1 / numero2
+
+    cadena = ""
+    
+    if basico == True:
+        cadena += f"Suma: {str(suma)}"
+        cadena += ("\n")
+        cadena += f"resta: {str(resta)}"
+    else:
+        cadena += ("\n")
+        cadena += f"Multiplicacion: {str(multi)}"
+        cadena += ("\n")
+        cadena += f"divicion: {str(divi)}"
+     
+    return cadena
+
+print(calculadora(10,3, False))
